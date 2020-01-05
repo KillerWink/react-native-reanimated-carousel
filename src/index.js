b68fba4974df6a0c3bfd0e09a4f6f66104ba2d3e
thomasgambina <thomas.gambina@gmail.com>
2020-01-05 19:00:46 +0100

    add exports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { CarouselContext, CarouselProvider, useCarouselEffect } from './CarouselContext';
+import ReanimatedCarousel from './ReanimatedCarousel';
+
+export {
+    CarouselContext,
+    CarouselProvider,
+    useCarouselEffect,
+    ReanimatedCarousel
+};
